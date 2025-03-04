d057ea981093a59ac3dfd83f8dcba93e2dea4daf
Valentina Vergel <115202426+VlVergel-U@users.noreply.github.com>
2025-03-04 11:50:21 -0500

    Update server.ts

--- src/config/server.ts
+++ src/config/server.ts
@@ -45,7 +45,7 @@ export default class Server {
 
     private middleware() {
         this.server.use(
-            cors()
+            cors({origin:config.url_cors})
         );
         this.server.use(express.json());
         this.server.use(express.urlencoded({extended:true}))
